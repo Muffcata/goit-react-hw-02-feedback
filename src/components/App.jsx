1b0a6619db2b5309687b482334f9e08e70f94734
Muffcata <marta.oched@gmail.com>
2023-03-16 20:39:04 +0100

    add class app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,30 @@
-import { Button } from './Button/Button';
+import React, { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div>
-      <Button text={'Good'} />
-      <Button text={'Neutral'} />
-      <Button text={'Bad'} />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    positive: 0,
+    neutral: 0,
+    negative: 0,
+  };
+  handleGoodIncrement(evt) {
+    if (evt.target.textContent === 'Good') {
+      this.setState({ good: this.state.good + 1 });
+    }
+    console.log(evt);
+  }
 
-export default App;
+  render() {
+    const { state } = this.props;
+
+    return (
+      <div>
+        <span>Please leave feedback</span>
+        <button type="button" onClick={this.handleGoodIncrement}>
+          Good {state}
+        </button>
+        <button type="button">Neutral{state}</button>
+        <button type="button">Bad {state}</button>
+      </div>
+    );
+  }
+}
