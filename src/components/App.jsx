7518bfd5c99ec51a4f9c892e0b3182783d6b3543
Muffcata <marta.oched@gmail.com>
2023-03-17 23:18:27 +0100

    step 1 complete - added interface with basic logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,24 +6,25 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
-  handleGoodIncrement(evt) {
-    if (evt.target.textContent === 'Good') {
-      this.setState({ good: this.state.good + 1 });
-      console.log(evt);
-    }
-  }
 
+  handleGoodIncrement = () => {
+    this.setState({ good: this.state.good + 1 });
+  };
+  handleNeutralIncrement = () => {
+    this.setState({ neutral: this.state.neutral + 1 });
+  };
+  handleBadIncrement = () => {
+    this.setState({ bad: this.state.bad + 1 });
+  };
   render() {
     const { good, neutral, bad } = this.state;
 
     return (
       <div>
         <h1>Please leave feedback</h1>
-        <button type="button" onClick={this.handleGoodIncrement}>
-          Good
-        </button>
-        <button type="button">Neutral</button>
-        <button type="button">Bad </button>
+        <button onClick={this.handleGoodIncrement}>Good</button>
+        <button onClick={this.handleNeutralIncrement}>Neutral</button>
+        <button onClick={this.handleBadIncrement}>Bad </button>
         <h2>Statistics</h2>
         <p>Good: {good}</p>
         <p>Neutral: {neutral}</p>
